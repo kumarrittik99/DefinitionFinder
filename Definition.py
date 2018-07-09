0b3d4cf87f3dfbf42acc19a5b90c850c9ed49503
Rittik Kumar <kumarrittik99@gmail.com>
2018-07-09 20:51:26 +0530

    Update Definition.py

--- Definition.py
+++ Definition.py
@@ -4,21 +4,23 @@ from urllib.request import Request
 
 
 url = "http://www.dictionary.com/browse/"
-word = input("Enter the word to find its definition.\n")
-url = url + word
+try:
+ word = input("Enter the word to find its definition.\n")
+ url = url + word
 
-newUrl = Request(url,headers={"User-Agent":"Mozilla/5.0"})
-data = urllib.request.urlopen(newUrl).read()
-actualData = data.decode("utf-8")
+ newUrl = Request(url,headers={"User-Agent":"Mozilla/5.0"})
+ data = urllib.request.urlopen(newUrl).read()
+ actualData = data.decode("utf-8")
 
 #print(actualData)
 #<meta name="description" content="Energy definition, the capacity for vigorous activity; available power: I eat chocolate to get quick energy. See more.">
 
-de = re.search('<meta name="description" content="',actualData)
-df = re.search(' See more.">',actualData)
+ de = re.search('<meta name="description" content="',actualData)
+ df = re.search(' See more.">',actualData)
 
 
-print(actualData[de.end():df.start()])
+ print(actualData[de.end():df.start()])
 
-
+except:
+  print("Enter a valid word!!! This word does not belong to the dictionary.")
 
